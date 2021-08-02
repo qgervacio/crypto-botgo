b39c7a3912bea98dd16f242af601c88c293ee83e
Quirino Gervacio <qgervacio@gmail.com>
2021-08-03 02:22:05 +0800

    feat: implemented buy and sell via market type

--- cbg/spot.go
+++ cbg/spot.go
@@ -3,6 +3,8 @@
 
 package cbg
 
+import "github.com/adshao/go-binance/v2"
+
 type SpotSvc struct {
 	SpotSpec *SpotSpec
 	BiapiSvc *BiapiSvc
@@ -15,10 +17,26 @@ func NewSpotSvc(ss *SpotSpec, bs *BiapiSvc) *SpotSvc {
 	}
 }
 
-func (s *SpotSvc) Buy() (string, error) {
-	return "Bought at xxx", nil
+func (s *SpotSvc) BuyMarket(quantity string) (*binance.CreateOrderResponse, error) {
+	q := quantity
+	if quantity == "" {
+		balance, err := s.BiapiSvc.GetBalance(s.SpotSpec.Market)
+		if err != nil {
+			return nil, err
+		}
+		q = balance
+	}
+	return s.BiapiSvc.BuyMarket(s.SpotSpec.Coin, s.SpotSpec.Market, q)
 }
 
-func (s *SpotSvc) Sell() (string, error) {
-	return "Sold at xxx", nil
+func (s *SpotSvc) SellMarket(quantity string) (*binance.CreateOrderResponse, error) {
+	q := quantity
+	if quantity == "" {
+		balance, err := s.BiapiSvc.GetBalance(s.SpotSpec.Coin)
+		if err != nil {
+			return nil, err
+		}
+		q = balance
+	}
+	return s.BiapiSvc.SellMarket(s.SpotSpec.Coin, s.SpotSpec.Market, q)
 }
